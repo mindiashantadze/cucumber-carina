67b61ecf44820c3c2199b9d84851c66a7b06d8cd
Mindia Shantadze <shantadze200202@gmail.com>
2023-05-04 15:25:11 +0400

    added test cases for other users

--- src/test/java/mshantadze/cucumber/ECommerceTest.java
+++ src/test/java/mshantadze/cucumber/ECommerceTest.java
@@ -1,30 +1,13 @@
 package mshantadze.cucumber;
 
-import com.qaprosoft.carina.core.foundation.AbstractTest;
 import com.qaprosoft.carina.core.foundation.cucumber.CucumberBaseTest;
 import io.cucumber.testng.CucumberOptions;
-import mshantadze.cucumber.pages.CartPage;
-import mshantadze.cucumber.pages.CheckoutPage;
-import mshantadze.cucumber.pages.HomePage;
-import mshantadze.cucumber.pages.LoginPage;
-import mshantadze.cucumber.utils.db.dao.ProductsDAO;
-import mshantadze.cucumber.utils.db.dao.UsersDAO;
-import mshantadze.cucumber.utils.db.models.Product;
-import mshantadze.cucumber.utils.db.models.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
 
 @CucumberOptions(
         features = "src/test/resources/features/ECommerce.feature",
         glue = "mshantadze.cucumber.stepdefinitions",
         plugin = {"pretty",
-                "html:target/cucumber-core-test-report",
+                "html:target/cucumber-core-test-report.html",
                 "pretty:target/cucumber-core-test-report.txt",
                 "json:target/cucumber-core-test-report.json",
                 "junit:target/cucumber-core-test-report.xml"}
--- src/test/resources/features/ECommerce.feature
+++ src/test/resources/features/ECommerce.feature
@@ -13,3 +13,20 @@ Feature: ECommerce Site
       Then User should see list of his/her products in checkout
       When User clicks on finish button
       Then User should see thank you message
+
+    Scenario: Standard user checkout
+      Given User logs in as "problem_user"
+      And User adds products to the cart
+      Then User should see label on cart icon with product count
+
+  Scenario: Standard user checkout
+    Given User logs in as "performance_glitch_user"
+    And User adds products to the cart
+    Then User should see label on cart icon with product count
+    And User goes to the cart page
+    Then User should see list of his/her products in cart
+    And User goes to the checkout page
+    When User enters required info
+    Then User should see list of his/her products in checkout
+    When User clicks on finish button
+    Then User should see thank you message
